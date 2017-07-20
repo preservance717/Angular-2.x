7eb8ce761e2fadf137ff775e435eec79d078b84e
preservance717 <GL717@outlook.com>
2017-07-20 21:57:08 +0800

    achieve function of adding new article

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Article} from "./article/article.model";
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works';
+  articles: Article[];
 
-  addArticle(title:HTMLInputElement, link:HTMLInputElement):boolean{
+  constructor() {
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular HomePage', 'http://angular.io', 1)
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title:${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value,link.value,0));
+    title.value = '';
+    link.value = '';
 
     return false;
   }
