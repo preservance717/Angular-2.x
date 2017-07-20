8d237f5294caad0c692d6dbe964f25516ba4160a
preservance717 <GL717@outlook.com>
2017-07-20 22:05:50 +0800

    show domain of article

--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -15,4 +15,13 @@ export class Article{
   voteDown() {
     this.votes -= 1;
   }
+
+  domain():string{
+    try{
+      const link: string = this.link.split("//")[1];
+      return link.split("//")[0];
+    }catch(err){
+      return null;
+    }
+  }
 }
