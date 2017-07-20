3ddee1149f3289555e495f00f161625fef6f93be
preservance717 <GL717@outlook.com>
2017-07-20 21:55:45 +0800

    encapsulate code of article's component

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,28 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {Article} from "./article.model";
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
+  host:{
+    class:'row'
+  }
 })
 export class ArticleComponent implements OnInit {
-  votes:number;
-  title:string;
-  link:string;
+  @Input()article:Article;
 
-  constructor() {
-    this.title = "Angular 2";
-    this.link = "http://angular.io";
-    this.votes = 10;
+  voteUp():boolean {
+    this.article.voteUp();
+    return false;
   }
 
-  voteUp(){
-   this.votes += 1;
+  voteDown():boolean {
+    this.article.voteDown();
+    return false;
   }
 
-  voteDown(){
-    this.votes -= 1;
-  }
   ngOnInit() {
   }
 
