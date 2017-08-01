d0fc68e6862510f3206b0d8cb794297b9e30ccb3
LG <GL717@outlook.com>
2017-08-01 23:12:58 +0800

    usage of ng-style

--- Angular_program_experience/src/app/app.component.ts
+++ Angular_program_experience/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  color: string;
+  fontSize: number;
 
+  apply(color: string, fontSize: number) {
+    this.color = color;
+    this.fontSize = fontSize;
+  }
 }
