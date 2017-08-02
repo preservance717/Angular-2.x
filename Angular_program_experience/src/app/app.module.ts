8aa24331449a05953092621dd63caf78236235ce
LG <GL717@outlook.com>
2017-08-02 23:09:12 +0800

    achieve SimpleHttpComponent

--- Angular_program_experience/src/app/app.module.ts
+++ Angular_program_experience/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {SwitchComponent} from "./switch/switch.component";
+import {SimpleHttpComponent} from "./simple_http/simple-http.component";
+import {HttpModule} from "@angular/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SwitchComponent
+    SwitchComponent,
+    SimpleHttpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Angular_program_experience/src/app/simple_http/simple-http.component.ts
@@ -0,0 +1,27 @@
+import {Component} from "@angular/core";
+import {Http, Response} from "@angular/http";
+@Component({
+  selector: 'simple-http',
+  template: `<h2> Basic Request</h2>
+<button type="button" (click)="makeRequest()">Make Request</button>
+<div *ngIf="loading">loading...</div>
+<pre>{{data | json}}</pre>`
+})
+
+export class SimpleHttpComponent {
+  data: Object;
+  loading: boolean;
+
+  constructor(private http: Http) {
+
+  }
+
+  makeRequest() {
+    this.loading = true;
+    this.http.request('http://jsonplaceholder.typicode.com/posts/1')
+      .subscribe((res: Response) => {
+        this.data = res.json();
+        this.loading = false;
+      })
+  }
+}
