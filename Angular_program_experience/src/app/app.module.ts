ae151f679336e5ed34d0f8998189a3d3a1f0bf01
LG <GL717@outlook.com>
2017-08-17 10:55:52 +0800

    usage of outlet in Router

--- Angular_program_experience/src/app/app.module.ts
+++ Angular_program_experience/src/app/app.module.ts
@@ -5,16 +5,35 @@ import { AppComponent } from './app.component';
 import {SwitchComponent} from "./switch/switch.component";
 import {SimpleHttpComponent} from "./simple_http/simple-http.component";
 import {HttpModule} from "@angular/http";
+import {Routes, RouterModule} from "@angular/router";
+import {PopComponent} from "./pop/pop.component";
 
+const appRoutes: Routes = [
+  { path: 'switch', component: SwitchComponent },
+  { path: 'simple', component: SimpleHttpComponent},
+  { path: 'pop', component: PopComponent,outlet:'popup'},
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
     SwitchComponent,
-    SimpleHttpComponent
+    SimpleHttpComponent,
+    PopComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Angular_program_experience/src/app/pop/pop.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+@Component({
+  selector: 'pop',
+  template: `<h1 style="color:green; z-index:5; box-shadow: 2px 2px 12px gray">pop</h1>`
+})
+
+export class PopComponent {
+}
