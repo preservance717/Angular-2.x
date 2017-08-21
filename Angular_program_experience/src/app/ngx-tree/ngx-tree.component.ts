7ee6062ceda83a2432c305ef99845ada58de85be
LG <GL717@outlook.com>
2017-08-21 18:27:54 +0800

    generate simple page of ngx-tree

--- Angular_program_experience/src/app/ngx-tree/ngx-tree.component.ts
+++ Angular_program_experience/src/app/ngx-tree/ngx-tree.component.ts
@@ -1,69 +1,154 @@
-import { Component, OnInit } from '@angular/core';
-import {ITreeOptions, TREE_ACTIONS} from "angular-tree-component";
+import {Component, OnInit} from '@angular/core';
+import {TreeNode, ITreeOptions, IActionMapping, TREE_ACTIONS, KEYS} from "angular-tree-component";
 
+const actionMapping:IActionMapping = {
+  mouse: {
+    contextMenu: (tree, node, $event) => {//右击
+      $event.preventDefault();//必须
+      alert(`context menu for ${node.data.name}`);
+    },
+    dblClick: (tree, node, $event) => {//双击左键
+      if (node.hasChildren) TREE_ACTIONS.TOGGLE_EXPANDED(tree, node, $event);
+    },
+    click: (tree, node, $event) => {
+      $event.shiftKey
+        ? TREE_ACTIONS.TOGGLE_SELECTED_MULTI(tree, node, $event)
+        : TREE_ACTIONS.TOGGLE_SELECTED(tree, node, $event);
+    }
+  },
+  keys: {
+    [KEYS.ENTER]: (tree, node, $event) => alert(`This is ${node.data.name}`)
+  }
+};
 @Component({
   selector: 'app-ngx-tree',
   templateUrl: './ngx-tree.component.html',
   styleUrls: ['./ngx-tree.component.css']
 })
 export class NgxTreeComponent implements OnInit {
-  nodes = [
-    {
-      id: 1,
-      name: 'root1',
-      // isExpanded: true,
-      children: [
-        { id: 2, name: 'child1' },
-        { id: 3, name: 'child2' }
-      ]
-    },
-    {
-      id: 4,
-      name: 'root2',
-      // isExpanded: true,
-      children: [
-        { id: 5, name: 'child2.1' },
+  nodes: any[];
+  nodes2 = [{name: 'root'}, {name: 'root2'}];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.nodes = [
         {
-          id: 6,
-          name: 'child2.2',
+          expanded: true,
+          name: 'root expanded',
+          subTitle: 'the root',
           children: [
-            { id: 7, name: 'subsub' }
+            {
+              name: 'child1',
+              subTitle: 'a good child',
+              hasChildren: false
+            }, {
+              name: 'child2',
+              subTitle: 'a bad child',
+              hasChildren: false
+            }
           ]
+        },
+        {
+          name: 'root2',
+          subTitle: 'the second root',
+          children: [
+            {
+              name: 'child2.1',
+              subTitle: 'new and improved',
+              uuid: '11',
+              hasChildren: false
+            }, {
+              name: 'child2.2',
+              subTitle: 'new and improved2',
+              children: [
+                {
+                  uuid: 1001,
+                  name: 'subsub',
+                  subTitle: 'subsub',
+                  hasChildren: false
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: 'asyncroot',
+          hasChildren: true
         }
-      ]
+      ];
+
+      // for(let i = 0; i < 4; i++) {
+      //   this.nodes.push({
+      //     name: `rootDynamic${i}`,
+      //     subTitle: `root created dynamically ${i}`,
+      //     children: new Array((i + 1) * 100).fill(null).map((item, n) => ({
+      //       name: `childDynamic${i}.${n}`,
+      //       subTitle: `child created dynamically ${i}`,
+      //       hasChildren: false
+      //     }))
+      //   });
+      // }
+    }, 1);
+  }
+
+  childrenCount(node: TreeNode): string {
+    return node && node.children ? `(${node.children.length})` : '';
+  }
+
+  onInitialized(tree) {
+    // tree.treeModel.getNodeById('11').setActiveAndVisible();
+  }
+
+  onEvent(event) {
+    // console.log(event);
+  }
+
+  go($event) {
+    $event.stopPropagation();
+    alert('this method is on the app component');
+  }
+
+  getChildren(node:any) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(this.asyncChildren.map((c) => {
+        return Object.assign({}, c, {
+          hasChildren: node.level < 5
+        });
+      })), 1000);
+    });
+  }
+
+  asyncChildren = [
+    {
+      name: 'child2.1',
+      subTitle: 'new and improved'
+    }, {
+      name: 'child2.2',
+      subTitle: 'new and improved2'
     }
   ];
 
-  options: ITreeOptions = {
-    displayField: 'name',
+  customTemplateStringOptions: ITreeOptions = {
+    displayField: 'subTitle',
     isExpandedField: 'expanded',
     idField: 'uuid',
-    actionMapping: {
-      mouse: {
-        dblClick: (tree, node, $event) => {
-          if (node.hasChildren) TREE_ACTIONS.TOGGLE_EXPANDED(tree, node, $event);
-        }
-      }
-    },
-    nodeHeight: 23,
+    getChildren: this.getChildren.bind(this),
+    actionMapping,
+    nodeHeight: 46,
     allowDrag: (node) => {
+      // console.log('allowDrag?');
       return true;
     },
     allowDrop: (node) => {
+      // console.log('allowDrop?');
       return true;
     },
     useVirtualScroll: true,
     animateExpand: true,
     animateSpeed: 30,
     animateAcceleration: 1.2
-  };
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  onEvent($event){
-    console.log($event);
   }
 }
